b3f1cbcedf4e9fb784edeaa2e17befabaa2c95af
Aruljothiviswa <aruljothi.viswa@gmail.com>
2022-04-07 20:15:26 +0800

    Card view Alignment added

--- React Projects/react-project1/src/7-4-2022/CardComp.js
+++ React Projects/react-project1/src/7-4-2022/CardComp.js
@@ -7,7 +7,7 @@ export default function CardComp(props) {
     return (
         <>
             <h1 className="functionClass">LG Product Details</h1>
-            <div class="row">
+            <div class="row" style={{padding:'2%'}}>
                 {tempArr.map((item) => (
                     <div class="col-sm-6 col-md-4 col-lg-3">
                         <div key={item.productId} className="card border-success mb-3" style={{ 'maxWidth': '300px' ,'min-height':'450px'}}>
