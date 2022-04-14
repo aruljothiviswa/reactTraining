f8e14ecc4da80cd7832dafe3b336701ea4a88c74
Aruljothiviswa <aruljothi.viswa@gmail.com>
2022-04-14 16:35:40 +0800

    NavBar dropdown list onClick event added

--- React Projects/react-project1/src/11-4-2022/NavBar.js
+++ React Projects/react-project1/src/11-4-2022/NavBar.js
@@ -31,9 +31,33 @@ export default function NavBar() {
                 <span href="#" className="navbar-brand d-sm-block d-xs-block d-md-none d-lg-none">&nbsp; J&E</span>
 
                 <div className="dropdown d-sm-block d-xs-block d-md-none d-lg-none">
-                    <span className="dropdown-toggler drop " type="button" data-toggle="dropdown">
-                        <span className="navbar-toggler-icon "></span>
-                        <span className='dropdown-menu custDrop' >
+                    <a href="#" className="nav-link dropdown-toggle" id="navbarDropdown"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false"><span className="navbar-toggler-icon"></span></a>
+                    <ul className='dropdown-menu' style={{ right: '0',left: 'auto'}} aria-labelledby='navbarDropdown'>
+                        <li><a href="#home" className="dropdown-item" onClick={getHome}>Home</a></li>
+                        <li><a href="#contact" className="dropdown-item" onClick={empDetails}>Employee Details</a></li>
+                        <li><a href="#register" className="dropdown-item" onClick={getRegister}>Registration</a></li>
+                        <li><a href="#product" className="dropdown-item" onClick={getProducts}>Fetch API</a></li>
+                        <li> <a href="#product" className="dropdown-item" onClick={getProductsFunction}>AXIOS API</a></li>
+                        <li><a href="#about" className="dropdown-item" onClick={getAbout}>About Us</a></li>
+                        <li> <a href="#contact" className="dropdown-item" onClick={getContact}>Contact Us</a></li>
+                    </ul>
+
+                    {/* <span className="dropdown-toggler drop " type="button" data-toggle="dropdown"> */}
+                    {/* <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        > <span className="navbar-toggler-icon"></span>
+                        </button> */}
+
+
+                    {/* <span className="navbar-toggler-icon "></span>
+                       <span className='dropdown-menu custDrop' id="navbarSupportedContent" >
                             <a href="#home" className="nav-item nav-link active" onClick={getHome}>Home</a>
                             <a href="#contact" className="nav-item nav-link active" onClick={empDetails}>Employee Details</a>
                             <a href="#register" className="nav-item nav-link active" onClick={getRegister}>Registration</a>
@@ -41,23 +65,16 @@ export default function NavBar() {
                             <a href="#product" className="nav-item nav-link active" onClick={getProductsFunction}>UseEffect Product</a>
                             <a href="#about" className="nav-item nav-link active" onClick={getAbout}>About Us</a>
                             <a href="#contact" className="nav-item nav-link active" onClick={getContact}>Contact Us</a>
-                        </span>
-                        {/* <ul className='dropdown-menu custDrop' >
-                            <li><a href="#home" className="nav-item nav-link active" onClick={getHome}><u>Home</u></a></li>
-                            <li><a href="#contact" className="nav-item nav-link active" onClick={empDetails}><u>Employee Details</u></a></li>
-                            <li><a href="#register" className="nav-item nav-link active" onClick={getRegister}><u>Registration</u></a></li>
-                            <li><a href="#about" className="nav-item nav-link active" onClick={getAbout}><u>About Us</u></a></li>
-                            <li><a href="#contact" className="nav-item nav-link active" onClick={getContact}><u>Contact Us</u></a></li>
-                         </ul> */}
-                    </span>
+                        </span> */}
+                    {/* </span> */}
                 </div>
                 <div className="collapse navbar-collapse hidden-sm hidden-xs" id="navbarNav">
                     <div className="navbar-nav ms-auto">
                         <a href="#home" className="nav-item nav-link active" onClick={getHome}>Home</a>
                         <a href="#contact" className="nav-item nav-link active" onClick={empDetails}>Employee Details</a>
                         <a href="#register" className="nav-item nav-link active" onClick={getRegister}>Registration</a>
-                        <a href="#product" className="nav-item nav-link active" onClick={getProducts}>Product API</a>
-                        <a href="#product" className="nav-item nav-link active" onClick={getProductsFunction}>UseEffect Product</a>
+                        <a href="#product" className="nav-item nav-link active" onClick={getProducts}>Fetch API</a>
+                        <a href="#product" className="nav-item nav-link active" onClick={getProductsFunction}>AXIOS API</a>
                         <a href="#about" className="nav-item nav-link active" onClick={getAbout}>About Us</a>
                         <a href="#contact" className="nav-item nav-link active" onClick={getContact}>Contact Us</a>
                     </div>
--- React Projects/react-project1/src/13-4-2022/FunctionalCompUseEffect.js
+++ React Projects/react-project1/src/13-4-2022/FunctionalCompUseEffect.js
@@ -2,17 +2,24 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 export default function FunctionalCompUseEffect() {
     const [data, setData] = useState([])
+    const [search, setSearch] = useState('')
+    var noDataFound = '';
+
     const style = {
         width: '100px',
         height: '80px'
+    }
+    const resetResult = () => {
+        setSearch('');
     }
     useEffect(() => {
         axios.get('https://fakestoreapi.com/products')
             .then(nodeData => setData(nodeData.data))
     }, [])
+
     return (
         <>
-            <div className="container custCon">
+            {/* <div className="container custCon">
                 <h1 className="functionClass">Product Details</h1>
                 <table className="table table-striped">
                     <tbody>
@@ -36,6 +43,55 @@ export default function FunctionalCompUseEffect() {
                         ))}
                     </tbody>
                 </table>
+            </div> */}
+
+            <div className="container custCon">
+                <div className="row">
+                    <div className="col">
+                        <input className="search" value={search} onChange={(event) => setSearch(event.target.value)} />&nbsp;
+                        <button className='btn btn-primary btn-sm' onClick={resetResult}>
+                            <i className="fa fa-refresh" aria-hidden="true"></i>
+                            <span className="d-none d-sm-none d-xs-none d-md-inline d-lg-inline"> Reset</span></button>
+                    </div>
+                </div>
+                <h1 className="functionClass">Product Details</h1>
+                <div className="row" style={{ padding: '2%' }}>
+                    {data.filter(value => {
+                        if (String(value.id).includes((search).toLowerCase()) || String((value.category).toLowerCase()).includes((search).toLowerCase()) ||
+                            String(value.price).includes((search).toLowerCase()) || String((value.description).toLowerCase()).includes((search).toLowerCase())) {
+                            return value;
+                        }
+                    }).map((item) => (
+                        <div key={item.id} className="col-sm-6 col-md-4 col-lg-3" style={{ margin: '2%' }}>
+                            <div className="card h-100" >
+                                <img className="card-img-top" src={item.image} alt="Card image" />
+                                <div className="card-body d-flex flex-column">
+                                    <h5 className="card-title">{item.id}-{item.category}</h5>
+                                    <div className="card-text">{item.description}<br></br><br />
+                                        {/* <p className="mt-auto ">
+                                            <p className="text-uppercase mb-0">Rs : {item.price}</p>
+                                        </p> */}
+                                    </div>
+                                    {/* <div class="card-footer text-muted mx-auto">Price : Rs.
+                                    </div> */}
+                                    <div className="card-footer mt-auto border border-primary">Price : Rs. {item.price}</div>
+                                </div>
+                            </div>
+                            {/* <div className="card h-100 border-success mb-3">
+                                <div className="card-header bg-transparent border-success">{item.id}-{item.category}</div>
+                                <img className="card-img-top" src={item.image} alt="Card"></img>
+                                <div className="card-body text-success">
+                                    <h5 className="card-title">{item.category}</h5>
+                                    <p className="card-text">{item.description}</p>
+                                </div>
+                                <div className="card-footer bg-transparent border-success">Price : Rs. {item.price}</div>
+                            </div> */}
+                        </div>
+                    ))
+                    }
+                    <div className="message">{noDataFound}</div>
+                </div>
+
             </div>
         </>
     )
